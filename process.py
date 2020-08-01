80d6b789092a43b2d94585d3fb520070d7a7dfab
tejesh-ch <66432619+tejesh-ch@users.noreply.github.com>
2020-08-01 15:47:06 -0500

    Update process.py

--- process.py
+++ process.py
@@ -1,5 +1,17 @@
 import os
 
+path = os.path.join(os.getcwd(), "Training")
+os.chdir(path)
+for child in os.listdir():
+    dir = os.getcwd()
+    os.chdir(os.path.join(dir, child))
+    i = 0
+    for label in os.listdir():
+        name = child + "_example_" + str(i+1) + ".jpg"
+        os.rename(label, name)
+        i += 1
+    os.chdir(dir)
+
 path = os.path.join(os.getcwd(), "Training", "no_tumor")
 os.chdir(path)
 i = 0
