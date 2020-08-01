c14490c79d3558108db39a47667183c7d9bac881
tejesh-ch <66432619+tejesh-ch@users.noreply.github.com>
2020-08-01 09:33:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ test_datagen = ImageDataGenerator(rescale = 1./255)
 # generator that will read pictures found and indefinitely generate batches of augmented image data
 train_generator = train_datagen.flow_from_directory(
         'Augmented/glioma_augmented',
-        target_size=(150, 150),
+        target_size = (150, 150),
         batch_size = batch_size,
         class_mode= 'binary')
 
@@ -56,8 +56,8 @@ validation_generator = test_datagen.flow_from_directory(
 
 model.fit_generator(
         train_generator,
-        steps_per_epoch=2000 // batch_size,
-        epochs=50,
-        validation_data=validation_generator,
-        validation_steps=800 // batch_size)
+        steps_per_epoch = 2000 // batch_size,
+        epochs = 50,
+        validation_data = validation_generator,
+        validation_steps = 800 // batch_size)
 model.save_weights('Output/glioma_tumor')
