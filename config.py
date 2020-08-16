96219a29e5e732cf26695725acad0b5330d65de6
tejesh-ch <66432619+tejesh-ch@users.noreply.github.com>
2020-08-16 13:25:31 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,42 +1,29 @@
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 
 datagen = ImageDataGenerator(
-        rotation_range = 10,
-        width_shift_range = 0.1,
-        height_shift_range = 0.1,
-        shear_range = 0.1,
-        zoom_range = 0.1,
+        rotation_range = 20,
+        width_shift_range = 0.2,
+        height_shift_range = 0.2,
+        shear_range = 0.2,
+        zoom_range = 0.2,
         horizontal_flip = True,
         fill_mode = 'nearest')
 
-for j in range (1, 827):
-    img = load_img(f'Training/glioma_tumor/glioma_tumor_example_{str(j)}.jpg')
+for j in range (1, 156):
+    img = load_img(f'MRI_Scans/has_tumor/has_tumor_example_{str(j)}.jpg')
     x = img_to_array(img)
     x = x.reshape((1,) + x.shape)
     i = 0
 
     generated_data = datagen.flow(x, batch_size=1,
-                                  save_to_dir='Augmented/glioma_augmented', save_prefix='glioma_tumor', save_format='jpeg')
+                                  save_to_dir='Augmented/has_tumor_augmented', save_prefix='has_tumor', save_format='jpg')
     for batch in generated_data:
         i += 1
-        if i > 20:
+        if i > 30:
             break
 
-for j in range (1, 823):
-    img = load_img(f'Training/meningioma_tumor/meningioma_tumor_example_{str(j)}.jpg')
-    x = img_to_array(img)
-    x = x.reshape((1,) + x.shape)
-    i = 0
-
-    generated_data = datagen.flow(x, batch_size=1,
-                                  save_to_dir='Augmented/meningioma_augmented', save_prefix='meningioma_tumor', save_format='jpeg')
-    for batch in generated_data:
-        i += 1
-        if i > 20:
-            break
-
-for j in range (1, 397):
-    img = load_img(f'Training/no_tumor/no_tumor_example{str(j)}.jpg')
+for j in range (1, 100):
+    img = load_img(f'MRI_Scans/no_tumor/no_tumor_example_{str(j)}.jpg')
     x = img_to_array(img)
     x = x.reshape((1,) + x.shape)
     i = 0
@@ -45,18 +32,6 @@ for j in range (1, 397):
                                   save_to_dir='Augmented/no_tumor_augmented', save_prefix='no_tumor', save_format='jpeg')
     for batch in generated_data:
         i += 1
-        if i > 27:
+        if i > 45:
             break
 
-for j in range (1, 828):
-    img = load_img(f'Training/pituitary_tumor/pituitary_tumor_example_{str(j)}.jpg')
-    x = img_to_array(img)
-    x = x.reshape((1,) + x.shape)
-    i = 0
-
-    generated_data = datagen.flow(x, batch_size=1,
-                                  save_to_dir='Augmented/pituitary_augmented', save_prefix='pituitary_tumor', save_format='jpeg')
-    for batch in generated_data:
-        i += 1
-        if i > 20:
-            break
